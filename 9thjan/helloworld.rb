cde86162710b2a91c499715593fa1b36ac5df63a
khushali25 <thakkarkhushali25@gmail.com>
2017-01-09 19:25:14 +0530

    Done

---
+++ 9thjan/helloworld.rb
@@ -0,0 +1,36 @@
+# print "hi" -----Single line comment
+
+=begin -----multiline comment
+puts "hi"
+=end
+
+#puts and print
+print "Ruby"
+print "Rails\n"
+puts "hello"
+puts "hello #hi"
+puts 'hii'
+
+#math operation
+puts "#{5+3-2}"
+puts 5+3-2
+puts "5+3-2"
+
+puts "#{4.2+4.2}"
+puts 4.2+4.2
+puts "4.2+4.2"
+
+#relational operation
+puts "5 is greater than 6 is #{5>6}"
+
+
+#for loop
+for i in 1..5
+puts "Value is #{i}"
+end
+
+for i in 1...5
+puts "Value is #{i}"
+end
+
+
