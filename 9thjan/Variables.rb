dc86915dc1834b1cb8eda125bdaa5b34c2b043c5
khushali25 <thakkarkhushali25@gmail.com>
2017-01-09 19:28:27 +0530

    Done

---
+++ 9thjan/Variables.rb
@@ -0,0 +1,30 @@
+#Variables Uses
+
+#int float
+a=10
+b=2.5
+c=a*b
+d=5
+e=d-b
+f=d/a
+g=e
+
+puts "#{a}#{b}"
+puts "#{a+b}"
+puts "#{c}"
+puts "#{e}"
+puts "#{f}"
+puts "#{g}"
+
+#string
+str="hi"
+str1="hey"
+var_int=10
+
+puts "#{str+" "+str1}"
+#Gives an error-----puts "#{str+var_int}"
+
+
+var1='hello'
+var2='world'
+puts "value of #{var1} and #{var2} is #{var1+var2}"
---
+++ 9thjan/string.rb
@@ -0,0 +1,30 @@
+a=10
+y="the value of a is #{a}"
+puts y
+puts "the value is #{y}"
+
+b=false
+puts "value is #{b}"
+
+
+c="hiii"
+d="heelloo"
+puts c+d
+
+f=c+d
+puts "#{f}"
+puts '#{f}'
+
+puts "."*3
+puts "a"*5
+puts "\."*5
+puts ".\."*4
+puts ".\.\."
+puts "\\\."*3
+puts '\\\.'*3
+puts "1\2"
+puts '1\2'
+puts "abc\abc"
+puts "abc\xbc"
+puts "abc\qbc"
+
