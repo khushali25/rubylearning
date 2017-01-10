3d04edff894ea44caea1e86dae0077d34d0c5c27
khushali25 <thakkarkhushali25@gmail.com>
2017-01-10 18:42:21 +0530

    Done

---
+++ 10thjan/to_methods.rb
@@ -0,0 +1,22 @@
+=begin
+
+a=20.reverse
+puts a
+
+=end
+
+#integer converted to string and reverse function
+a=20.to_s.reverse
+puts a
+
+#Numerical string converted to integer 
+b="50"
+c=b.to_i
+puts c
+
+#Any alphabetical string converted to integer will gives an output zero.
+d="abc"
+e=d.to_i
+puts e
+
+
