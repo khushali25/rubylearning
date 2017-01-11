23a38cf382c28a70d292f6a8d84503d35cc9cb6c
khushali25 <thakkarkhushali25@gmail.com>
2017-01-11 19:29:26 +0530

    done

---
+++ 11thjan/array&hash.rb~
@@ -0,0 +1,16 @@
+array1=[1,10.20,"hello","world"]
+array1.each do|i|
+puts i
+end
+
+hash={"one" => 0xf00,"two" => 0x0f00}
+hash.each do |key, value|
+   print "Key=", key, " and value=", value ,"\n"
+end
+
+sum=0
+(1..5).each do|i|
+sum=sum+i
+
+end
+print sum,"\n"
---
+++ 11thjan/escapesequence.rb
@@ -0,0 +1,36 @@
+puts "I am 5'2\" tall"
+puts 'I am 5\'2" tall'
+puts "\t\tI am tabbed in"
+puts "I am 5\ 2\ tall"
+
+puts a="""
+\t*hello
+\t*ruby
+"""
+
+Month="jan\nfeb\nmarch\n"
+print Month
+
+Days="sun mon tue wed thu"
+puts Days
+
+puts %q{
+There's something going on here.
+With the three double-quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+}
+
+puts "hello\b\b\b\\bworld"
+
+puts "hellohfhgff\rworld"
+
+puts "\a hello world \a" 
+
+puts "\sh\sello\sworld"
+
+puts "hello\vworld\v hiii"
+
+#puts "\xn55"
+
+#puts "\u55"
