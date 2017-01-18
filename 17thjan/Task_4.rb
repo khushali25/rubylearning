7f34a1b79174f964a29289c7d437c4527e0b5e10
khushali25 <thakkarkhushali25@gmail.com>
2017-01-18 12:14:27 +0530

    Update Task_4.rb

--- 17thjan/Task_4.rb
+++ 17thjan/Task_4.rb
@@ -1,12 +1,11 @@
 
 5.downto(1).each do |i|
-	
-	print ("*"*i).ljust(5)
+	print ("*"*i).ljust(5).chop
 	print ("*"*i).rjust(4)
 	puts "\n"
 end
 2.upto(5).each do |i|
-	print ("*"*i).ljust(5)
+	print ("*"*i).ljust(5).chop
 	print ("*"*i).rjust(4)
 	puts "\n"
 end
